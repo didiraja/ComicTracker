83f9ecca76d534d2b6b1d84ef94c3b6c83a4316b
didiraja <didiraja@hotmail.com>
2023-06-17 18:53:08 -0300

    refactor: 💡 additional config

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.port;
+
+app.use(express.json())
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("Express + Typescript Server running");

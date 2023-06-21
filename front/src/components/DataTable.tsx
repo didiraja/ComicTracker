8fe2ad08b614b8bd0776bb6ed37b0b5ca5dbc39c
didiraja <didiraja@hotmail.com>
2023-06-21 16:21:48 -0300

    refactor: 💡 minor upgrades

--- front/src/components/DataTable.tsx
+++ front/src/components/DataTable.tsx
@@ -3,7 +3,7 @@ import { InterfaceComic } from '../App';
 
 const DataTable = ({ data }: { data: InterfaceComic[] }) => {
   return (
-    <div className="bg-gray-900 rounded-lg overflow-hidden border border-neutral-500">
+    <div className="bg-gray-900 rounded-lg overflow-hidden border border-gray-700">
       <div className="sm:-mx-6 lg:-mx-8">
         <div className="min-w-full sm:px-6 lg:px-8">
           <div>
@@ -22,7 +22,7 @@ const DataTable = ({ data }: { data: InterfaceComic[] }) => {
               <tbody>
                 {
                   data.map((item: InterfaceComic) => (
-                    <tr>
+                    <tr key={item.id}>
                       <td className="whitespace-nowrap px-6 py-4">{item.publisher}</td>
                       <td className="whitespace-nowrap px-6 py-4">{item.title}</td>
                       <td className="whitespace-nowrap px-6 py-4">{item.issue}</td>
@@ -30,12 +30,8 @@ const DataTable = ({ data }: { data: InterfaceComic[] }) => {
                       <td className="whitespace-nowrap px-6 py-4">{item.writer}</td>
                       <td className="whitespace-nowrap px-6 py-4">{item.illustrator}</td>
                       <td className="whitespace-nowrap px-6 py-4">
-                        <button type="button" className="rounded bg-indigo-500 p-2 mr-2 text-sm uppercase leading-normal text-white">
-                          Edit
-                        </button>
-                        <button type="button" className="rounded bg-red-500 p-2 text-sm uppercase leading-normal text-white">
-                          Delete
-                        </button>
+                        <button type="button" className="rounded bg-indigo-500 hover:bg-indigo-600 p-2 mr-2 text-sm uppercase leading-normal text-white">Edit</button>
+                        <button type="button" className="rounded bg-red-500 hover:bg-red-600 p-2 text-sm uppercase leading-normal text-white">Delete</button>
                       </td>
                     </tr>
                   ))

7a433bc0680a95211bdb249912b3a873be54255c
didiraja <didiraja@hotmail.com>
2023-07-14 16:59:37 -0300

    refactor: 💡 navbar and publisher input on FormEntry

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,92 +1,81 @@
 import { useEffect, useState } from 'react'
-import { fetchData, addComic, IComicData, removeComic } from './services'
+import { fetchData, addComic, type IComicData, removeComic } from './services'
 // import FormComic from './components/FormComic'
 import FormEntry from './components/FormEntry'
 import DataTable from './components/DataTable'
 import './App.scss'
 
-export type IComic = {
-  id: number;
-  publisher: string;
-  title: string;
-  issue: number;
-  year: number;
-  writer: string;
-  illustrator: string;
+export interface IComic {
+  id: number
+  publisher: string
+  title: string
+  issue: number
+  year: number
+  writer: string
+  illustrator: string
 }
 
-export type ICategory = {
-  id: number;
-  name: string;
+export interface ICategory {
+  id: number
+  name: string
 }
 
-export type DashboardData = {
-  comics: IComic[];
-  publishers: ICategory[];
-  writers: ICategory[];
-  illustrators: ICategory[];
+export interface DashboardData {
+  comics: IComic[]
+  publishers: ICategory[]
+  writers: ICategory[]
+  illustrators: ICategory[]
 }
 
-function App() {
-
+function App(): JSX.Element {
   const INITIAL_DASH = {
     comics: [],
     publishers: [],
     writers: [],
-    illustrators: [],
+    illustrators: []
   }
 
-  const [isLoading, setLoading] = useState(false);
-  const [errorLoading, setErrLoading] = useState(false);
+  const [isLoading, setLoading] = useState(false)
+  const [errorLoading, setErrLoading] = useState(false)
 
-  const [dashData, setDashData] = useState<DashboardData>(INITIAL_DASH);
+  const [dashData, setDashData] = useState<DashboardData>(INITIAL_DASH)
 
-  const fetchDash = async () => {
+  const fetchDash = async (): Promise<void> => {
+    setLoading(true)
 
-    setLoading(true);
-    
     try {
+      const data = await fetchData(`${import.meta.env.DEV ? 'http://localhost:5200' : 'https://comictracker.onrender.com'}/dashboard`)
 
-      const data = await fetchData(`${import.meta.env.DEV ? 'http://localhost:5200' : 'https://comictracker.onrender.com'}/dashboard`);
-      
-      setDashData(data);
-      setLoading(false);
+      setDashData(data)
+      setLoading(false)
     } catch (error) {
-
-      setErrLoading(true);
-      setLoading(false);
-      console.error("Error:", error);
-
-      return {}
+      setErrLoading(true)
+      setLoading(false)
+      console.error('Error:', error)
     }
   }
 
-  const newComicFlow = async (comic: IComicData) => {
+  const newComicFlow = async (comic: IComicData): Promise<void> => {
     try {
       await addComic(`${import.meta.env.DEV ? 'http://localhost:5200' : 'https://comictracker.onrender.com'}/comics`, comic)
-    }
-    catch (e) {
-      console.log(e);
-    }
-    finally {
-      fetchDash()
+    } catch (e) {
+      console.log(e)
+    } finally {
+      await fetchDash()
     }
   }
 
-  const newEntryFlow = async (entry: any) => {
-    
-    console.log(entry);
+  const newEntryFlow = async (entry: any): Promise<void> => {
+    console.log(entry)
   }
 
-  const removeComicFlow = async (id: number) => {
+  const removeComicFlow = async (id: number): Promise<void> => {
     try {
       await removeComic(id)
-    }
-    catch (e) {
-      console.log(e);
-    }
-    finally {
-      fetchDash()
+    } catch (e) {
+      console.log(e)
+    } finally {
+      await fetchDash()
     }
   }
 
--- front/src/components/FormEntry.tsx
+++ front/src/components/FormEntry.tsx
@@ -1,76 +1,138 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import './FormEntry.scss';
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import './FormEntry.scss'
+import { useState } from 'react'
 
-export type IEntry = {
-  name: string;
-  writer: boolean;
-  illustrator: boolean;
+export interface IEntry {
+  name: string
+  publisher: string
+  writer: boolean
+  illustrator: boolean
 }
 
-const FormEntry = ({ submit }: { submit: (entry: IEntry) => void }) => {
+const FormEntry = ({ submit }: { submit: (entry: IEntry) => void }): JSX.Element => {
+  const labels = ['Writer/Illustrator', 'Publisher']
+  const [navActive, setActive] = useState(labels[0])
+
+  const NavBar = ({ reset }: { reset: () => void }): JSX.Element => {
+    return (
+      <div className="nav-wrapper">
+        {
+          labels.map((nav) => (
+            <button className={`nav-btn ${(navActive === nav) ? 'active' : ''}`} onClick={() => {
+              setActive(nav)
+
+              reset()
+            }} key={nav}>
+              {nav}
+            </button>
+          ))
+        }
+      </div>
+    )
+  }
 
   return (
-    <div className="form-wrapper">
-      <Formik
-        initialValues={{
-          name: '',
-          writer: false,
-          illustrator: false,
-        }}
-        validate={values => {
+    <>
+      <div className="form-wrapper">
+        <Formik
+          initialValues={{
+            name: '',
+            publisher: '',
+            writer: false,
+            illustrator: false
+          }}
+          validate={values => {
+            const errors: any = {}
 
-          const errors: any = {};
+            if (navActive === labels[0]) {
+              // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+              if (!values.name) {
+                errors.name = 'Entry name is required'
+              }
 
-          if (!values['name']) {
-            errors.name = "Entry name is required";
-          }
+              if (!values.writer && !values.illustrator) {
+                errors.writer = 'Writer or Illustrator is required'
+              }
+            }
 
-          if (!values['writer'] && !values['illustrator']) {
-            errors.writer = "Writer or Illustrator is required"
-          }
+            if (navActive === labels[1]) {
+              // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+              if (!values.publisher) {
+                errors.publisher = 'Publisher name is required'
+              }
+            }
 
-          return errors;
-        }}
-        onSubmit={(values, { resetForm }) => {
+            return errors
+          }}
+          onSubmit={(values, { resetForm }) => {
+            submit(values)
+            resetForm()
+          }}
+        >
+          {({ resetForm }) => {
+            return (
+              <>
+                <NavBar reset={resetForm} />
 
-          submit(values);
+                <Form id="form">
+                  <div className='form-grid'>
 
-          resetForm();
-        }}
-      >
-        <Form id="form">
-          <div className='form-grid'>
-            <div>
-              <label className="label" htmlFor="title">Name</label>
-              <Field className="form-default" type="text" name="name"
-                placeholder="Takehiko Inoue" />
-            </div>
+                    {
+                      (navActive === labels[0])
+                        ? <>
+                          <div>
+                            <label className="label" htmlFor="name">Name</label>
+                            <Field className="form-default" type="text" name="name"
+                              placeholder="Takehiko Inoue" />
+                          </div>
 
-            <div className="option-wrapper flex justify-evenly items-center">
-              <div className="entry">
-                <Field type="checkbox" name="writer" label="Writer" />
-                <label className="label-check" htmlFor="writer">Writer</label>
-              </div>
+                          <div className="option-wrapper flex justify-evenly items-center">
+                            <div className="entry">
+                              <Field type="checkbox" name="writer" label="Writer" />
+                              <label className="label-check" htmlFor="writer">Writer</label>
+                            </div>
 
-              <div className="entry">
-                <Field type="checkbox" name="illustrator" label="Illustrator" />
-                <label className="label-check" htmlFor="illustrator">Illustrator</label>
-              </div>
-            </div>
-          </div>
-          
-          <ErrorMessage name="name" component="div" className="form-error" />
-          <ErrorMessage name="writer" component="div" className="form-error" />
+                            <div className="entry">
+                              <Field type="checkbox" name="illustrator" label="Illustrator" />
+                              <label className="label-check" htmlFor="illustrator">Illustrator</label>
+                            </div>
+                          </div>
+                        </>
+                        : null
 
-          <button
-            type="submit"
-            className='btn-submit'>
-            Add New Entry
-          </button>
-        </Form>
-      </Formik>
-    </div>
-  );
+                    }
+
+                    {
+                      (navActive === labels[1])
+                        ? <>
+                          <div>
+                            <label className="label" htmlFor="publisher">Publisher</label>
+                            <Field className="form-default" type="text" name="publisher"
+                              placeholder="Dark Horse" />
+                          </div>
+                        </>
+                        : null
+                    }
+                  </div>
+
+                  <ErrorMessage name="name" component="div" className="form-error" />
+                  <ErrorMessage name="writer" component="div" className="form-error" />
+                  <ErrorMessage name="publisher" component="div" className="form-error" />
+
+                  <button
+                    type="submit"
+                    className='btn-submit'>
+                    Add New Entry
+                  </button>
+                </Form>
+              </>
+            )
+          }}
+
+        </Formik>
+      </div>
+    </>
+  )
 }
 
-export default FormEntry;
+export default FormEntry

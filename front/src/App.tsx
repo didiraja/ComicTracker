1c80007eccc192c00d29879f28a900930ddac32e
didiraja <didiraja@hotmail.com>
2023-07-15 18:57:58 -0300

    feat: 🎸 navbar implemented

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,16 +1,35 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route, NavLink, useLocation } from 'react-router-dom';
 import Home from './routes/Home';
 import NewEntry from './routes/NewEntry';
 import NoMatch from './routes/NoMatch';
 import './App.scss'
+import { ReactElement } from 'react';
 
 function App(): JSX.Element {
 
+  function handleClick(e: MouseEvent, to: string) {
+
+    const location = useLocation();
+
+    if (location.pathname === to) {
+      e.preventDefault();
+    }
+  }
+
+  const NavCustom = ({ route, children }: { route: string, children: string }) => {
+    return <NavLink to={route} className="nav" onClick={() => handleClick}>{children}</NavLink>
+  }
+
   return (
     <>
-      <h1 className="title-main">ComicTracker</h1>
-
       <HashRouter>
+        <div className="navbar">
+          <NavCustom route="/">New Comic</NavCustom>
+          <NavCustom route="/entry">New Entry</NavCustom>
+        </div>
+
+        <h1 className="title-main">ComicTracker</h1>
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/entry" element={<NewEntry />} />

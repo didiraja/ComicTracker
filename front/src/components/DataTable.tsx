f13b055ce160bffdb527ffd3e9c46002f33ab538
didiraja <didiraja@hotmail.com>
2023-07-17 18:44:44 -0300

    feat: 🎸 icons as components

--- front/src/components/DataTable.tsx
+++ front/src/components/DataTable.tsx
@@ -1,4 +1,8 @@
 import { IComic, ICategory } from '../routes/Home';
+import Close from './icon/Close';
+import Load from './icon/Load';
+import Edit from './icon/Edit';
+import Delete from './icon/Delete';
 import './DataTable.scss';
 
 export type ComicOrEntry = IComic | ICategory
@@ -12,9 +16,7 @@ const DataTable = (
   if (errorLoading) {
     return (
       <div className="error-loading">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg>
+        <Close />
 
         <p className="error-title"> Nothing here, only anti-matter</p>
       </div>
@@ -24,10 +26,7 @@ const DataTable = (
   if (isLoading) {
     return (
       <div className="table-is-loading">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
-          className="w-6 h-6 animate-spin">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-        </svg>
+        <Load />
 
         <p className="loading-title">Fetching content...</p>
       </div>
@@ -57,7 +56,6 @@ const DataTable = (
                       </th>
                     )) : null
                 }
-                {/* <th scope="col" className="table-head">Meta</th> */}
               </tr>
             </thead>
             <tbody>
@@ -73,12 +71,14 @@ const DataTable = (
                     {
                       remove ?
                         <td className="table-item">
-                          <button type="button" className="btn edit-btn">Edit</button>
+                          <button type="button" className="btn edit-btn">
+                            <Edit />
+                          </button>
                           <button
                             type="button"
                             className="btn delete-btn"
                             onClick={() => remove(item.id)}>
-                            Delete
+                            <Delete />
                           </button>
                         </td>
                         : null
